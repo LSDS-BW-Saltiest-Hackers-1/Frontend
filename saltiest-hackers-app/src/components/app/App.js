5ce828b6f50370b502cef0b45393bddf7fb3c5d2
Matthew Molloy <iatechristmas@gmail.com>
2020-05-27 11:14:34 -0700

    finished form

--- saltiest-hackers-app/src/components/app/App.js
+++ saltiest-hackers-app/src/components/app/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import './App.css';
-import Home from '../pages/HomePage/Home.js';
-import NavBar from '../NavBar/NavBar.js';
-import UserHome from '../pages/UserHome/UserHome.js';
+import Home from "../Pages/HomePage/Home.js";
+import NavBar from "../NavBar/NavBar.js";
+import UserHome from "../Pages/UserHome/UserHome.js";
 
-import PrivateRoute from '../PrivateRoute/PrivateRoute.js';
+import PrivateRoute from "../PrivateRoute/PrivateRoute.js";
 
 function App() {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
 
   return (
-    <div className='App'>
+    <div className="App">
       <NavBar />
       <Switch>
-        {(token ? <PrivateRoute exact path='/' component={UserHome}/> : <Route exact path='/' component={Home}/>)}
+        {token ? (
+          <PrivateRoute exact path="/" component={UserHome} />
+        ) : (
+          <Route exact path="/" component={Home} />
+        )}
       </Switch>
     </div>
   );
